2a47c845d5b80082459d9707c35c2a6b2566bc4a
Jose Antonio Gonzales <jzgonzales@gmail.com>
2020-07-18 12:44:09 -0500

    Se prueba el circuito OK, lectura de cada bit de entrada OK

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,36 @@
-// Pin connections for Mega2560:
+/* version 4.x del tablero de LEDS 8x8 que incorpora:
+* 8 circuitos de desplazamiento 74HC595 para escritura
+* 8 registros de desplazamiento 74LS165 para lectura
+* Utilizacion del protocolo SPI 
+* @autor: Jose Antonio Gonzales jzgonzales@gmail.com
+* @date: julio 2020
+*/
 
-// Chip pin 1 (/PL)  goes to LATCH (D9)  (or any other pin by changing LATCH below)
-// Chip pin 2 (CP)   goes to SCK   (D52)
-// Chip pin 9 (Q7)   goes to MISO  (D50)
-
+/*
+******************************************** Librerias ********************************************
+*/
 
 #include <SPI.h>
 
+/*
+******************************************** Variables y constantes ********************************************
+*/
+
 const byte LATCH = 9;
 const int rowSize = 8;
+
+byte switchArray1 = B00000000;
+byte switchArray2 = B00000000;
+int bin = 0;
+
 int state[rowSize] = { 0,0,0,0,0,0,0,0 };
 
-void setup ()
-{
-  SPI.begin ();
-  Serial.begin (115200);
-  Serial.println ("Begin switch test.");
-  pinMode (LATCH, OUTPUT);
-  digitalWrite (LATCH, HIGH);
-}
+/*
+******************************************** Funciones ********************************************
+*/
 
-byte switchArray1;
-byte switchArray2; // previous state
-
-void loop ()
-{
-  digitalWrite (LATCH, LOW);    // pulse the parallel load latch
-  digitalWrite (LATCH, HIGH);
+void viejaFuncion(){
   
-  SPI.setBitOrder(LSBFIRST);
   switchArray1 = SPI.transfer(0);
   switchArray2 = SPI.transfer(0);
 
@@ -48,5 +50,50 @@ void loop ()
   Serial.print(" Hexadecimal : ");
   Serial.println(switchArray2, HEX);
 
-  delay (30);   // debounce
-}  // end of loop
+}
+
+int inputArrayState(){
+
+  digitalWrite (LATCH, LOW); 
+  digitalWrite (LATCH, HIGH);
+
+  SPI.setBitOrder(LSBFIRST);
+  switchArray1 = SPI.transfer(0);
+  switchArray2 = SPI.transfer(0);
+  for(byte i = 0; i<rowSize; i++){
+
+    bin = bitRead(switchArray1,i); 
+    Serial.print((String)"Estado pin " +  i + " : " );
+    Serial.println(bin);
+    
+  }
+
+  Serial.println("Arreglo: 1");
+  return bin;
+  delay (5000);  
+}
+
+/*
+******************************************** Funciones ejecucion de Arduino ********************************************
+*/
+
+//funcion de inicializacion general Arduino - Hardware y software
+
+void setup ()
+{
+  SPI.begin ();
+  Serial.begin (115200);
+  Serial.println ("Begin switch test.");
+  pinMode (LATCH, OUTPUT);
+  digitalWrite (LATCH, HIGH);
+}
+ 
+
+
+//funcion ciclica Arduino 
+
+void loop ()
+{
+  inputArrayState();
+
+} 
